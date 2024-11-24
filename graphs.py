3374611d8a3aa8c9e0c6436a6b90a365ee6db44f
Arielle5785 <arielle.benadi@gmail.com>
2024-11-24 18:01:45 +0200

    updated the graph based on the data in attendence

--- graphs.py
+++ graphs.py
@@ -24,9 +24,13 @@ def graph_rep():
     db_url = 'postgresql+pg8000://postgres:3443@localhost:5433/bootcamp'
     engine = create_engine(db_url)
 
-    query = '''SELECT username, COUNT(done) as session_count FROM session WHERE done is TRUE
-                GROUP BY username'''
-
+    # query = '''SELECT date,COUNT(attendency) as session_count FROM attendance WHERE attendency is TRUE
+    #             GROUP BY date'''
+    query = '''SELECT 
+        CASE WHEN attendency IS TRUE THEN 'Completed' ELSE 'Not completed' END AS status,
+        COUNT(*) AS count
+    FROM attendance
+    GROUP BY status'''
 
     df = pd.read_sql(query, engine)
 
@@ -37,15 +41,23 @@ def graph_rep():
     engine.dispose()
 
     plt.figure(figsize=(10, 6))
-    if 'session_count' in df.columns:
-        plt.bar(df['username'], df['session_count'], color='skyblue')
-        plt.title('Number of Sessions per User', fontsize=16)
-        plt.xlabel('Username', fontsize=14)
-        plt.ylabel('Session Count', fontsize=14)
-        plt.grid(axis='y', linestyle='-.', alpha=0.7)
-        plt.xticks(rotation=45, fontsize=10)
-        plt.tight_layout()
+    # if 'session_count' in df.columns:
+        # plt.bar(df['date'], df['session_count'], color='skyblue')
+        # plt.title('Completed Sessions', fontsize=16)
+        # plt.xlabel('Date', fontsize=14)
+        # plt.ylabel('Completed', fontsize=14)
+        # plt.grid(axis='y', linestyle='-.', alpha=0.7)
+        # plt.xticks(rotation=45, fontsize=10)
+        # plt.tight_layout()
+        # plt.show()
+    if 'count' in df.columns and 'status' in df.columns:
+        statuses = df['status']
+        counts = df['count']
+        plt.figure(figsize=(8, 8))
+        plt.pie(counts, labels=statuses, autopct='%1.1f%%', startangle=90, colors=['lightblue', 'lightcoral'])
+        plt.title('Proportion of Attendencies (Completed vs Not completed)', fontsize=16)
         plt.show()
+
     else:
         print("Column 'session_count' not found. Check your SQL query or DataFrame.")
 graph_rep()
